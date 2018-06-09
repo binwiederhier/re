07278c360925b6a8fb4364f1fb2a71aa75f6dba5
Philipp Heckel <pheckel@datto.com>
2018-06-09 17:38:45 -0400

    Restructure

--- src/re.go
+++ src/re.go
@@ -9,77 +9,88 @@ import (
 	"strings"
 )
 
+var needle string
+var replacement string
+var dirs []string
+var verbose *bool
+var apply *bool
+var replaced int
+
+func walk(path string, info os.FileInfo, err error) error {
+	if info.IsDir() {
+		return nil
+	}
+
+	contents, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		fmt.Errorf("E Error reading %s. Skipping.\n", path)
+		return nil
+	}
+
+	oldContents := string(contents)
+	newContents := strings.Replace(oldContents, needle, replacement, -1)
+
+	if oldContents != newContents {
+		replaced++
+		fmt.Println("+", path)
+
+		if *apply {
+			ioutil.WriteFile(path, []byte(newContents), info.Mode())
+		}
+	} else {
+		if *verbose {
+			fmt.Println("-", path)
+		}
+	}
+
+	return nil
+}
+
 func main() {
-	verbose := flag.Bool("v", false, "Print files verbosely")
-	force := flag.Bool("f", false, "Apply changes without asking")
+	verbose = flag.Bool("v", false, "Verbose output")
+	apply = flag.Bool("f", false, "Apply changes")
+
+	flag.Usage = func() {
+		fmt.Println("Syntax: re [options] SEARCH REPLACEMENT [DIR ...]")
+		fmt.Println()
+		fmt.Println("Options:")
+		flag.PrintDefaults()
+	}
 
 	flag.Parse()
 
-	if len(flag.Args()) < 3 {
-		flag.PrintDefaults()
+	if len(flag.Args()) < 2 {
+		flag.Usage()
 		os.Exit(1)
 	}
 
-	var dirs []string
-	needle := flag.Arg(0)
-	replacement := flag.Arg(1)
+	needle = flag.Arg(0)
+	replacement = flag.Arg(1)
 
-	if len(flag.Args()) >= 2 {
+	if len(flag.Args()) > 2 {
 		dirs = flag.Args()[2:]
 	} else {
 		dirs = append(dirs, ".")
 	}
 
-	if !*force {
+	if !*apply {
 		fmt.Println("No changes will be applied unless -f is given.")
 	}
-
-	replaced := 0
 
 	for _, dir := range dirs {
 		if *verbose {
 			fmt.Println("Scanning", dir)
 		}
 
-		err := filepath.Walk(dir, func (path string, info os.FileInfo, err error) error {
-			if err != nil {
-				fmt.Errorf("E Error scanning dir %s. Skipping.\n", dir)
-				return nil
-			}
-
-			if !info.IsDir() {
-				contents, err := ioutil.ReadFile(path)
-
-				if err != nil {
-					fmt.Errorf("E Error reading %s. Skipping.\n", path)
-				} else {
-					oldContents := string(contents)
-					newContents := strings.Replace(oldContents, needle, replacement, -1)
-
-					if oldContents != newContents {
-						replaced++
-						fmt.Println("+", path)
-
-						if *force {
-							ioutil.WriteFile(path, []byte(newContents), info.Mode())
-						}
-					} else {
-						if *verbose {
-							fmt.Println("-", path)
-						}
-					}
-				}
-			}
-
-			return nil
-		})
+		err := filepath.Walk(dir, walk)
 
 		if err != nil {
-			panic(err)
+			fmt.Errorf("Cannot scan directory: %s; Skipping.\n", err.Error())
 		}
 	}
 
-	if !*force {
+	if !*apply {
 		fmt.Printf("%d file(s) WOULD have been updated.\n", replaced)
 	} else {
 		fmt.Printf("%d file(s) were updated.\n", replaced)
