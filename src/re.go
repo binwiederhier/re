0b923f1eac9ac464fb108a9ab17924dc43175a08
Philipp Heckel <pheckel@datto.com>
2018-06-09 18:28:42 -0400

    Include and exclude support

--- src/re.go
+++ src/re.go
@@ -7,20 +7,51 @@ import (
 	"path/filepath"
 	"io/ioutil"
 	"strings"
+	"regexp"
 )
 
 var needle string
 var replacement string
 var dirs []string
-var verbose *bool
-var apply *bool
 var replaced int
+var excludes []string
+var includes []regexp.Regexp
+var flagVerbose *bool
+var flagApply *bool
+var flagExcludes *string
+var flagIncludes *string
 
 func walk(path string, info os.FileInfo, err error) error {
+	// Skip if in exclude list
+	basename := filepath.Base(path)
+
+	for _, exclude := range excludes {
+		if basename == exclude {
+			return filepath.SkipDir
+		}
+	}
+
+	if len(includes) > 0 {
+		matches := false
+		
+		for _, include := range includes {
+			if include.Match([]byte(path)) {
+				matches = true
+				break
+			}
+		}
+		
+		if (!matches) {
+			return nil
+		}
+	}
+
+	// Skip folders
 	if info.IsDir() {
 		return nil
 	}
 
+	// Read contents
 	contents, err := ioutil.ReadFile(path)
 
 	if err != nil {
@@ -28,6 +59,7 @@ func walk(path string, info os.FileInfo, err error) error {
 		return nil
 	}
 
+	// Compare contents
 	oldContents := string(contents)
 	newContents := strings.Replace(oldContents, needle, replacement, -1)
 
@@ -35,11 +67,11 @@ func walk(path string, info os.FileInfo, err error) error {
 		replaced++
 		fmt.Println("+", path)
 
-		if *apply {
+		if *flagApply {
 			ioutil.WriteFile(path, []byte(newContents), info.Mode())
 		}
 	} else {
-		if *verbose {
+		if *flagVerbose {
 			fmt.Println("-", path)
 		}
 	}
@@ -47,9 +79,33 @@ func walk(path string, info os.FileInfo, err error) error {
 	return nil
 }
 
+func parseRegexList(s string) []regexp.Regexp {
+	stringList := strings.Split(s, ",")
+	result := make([]regexp.Regexp, 0)
+
+	for i := 0; i < len(stringList); i++ {
+		pattern := regexp.QuoteMeta(stringList[i])
+		pattern = strings.Replace(pattern, "\\*", ".*", -1)
+		pattern = strings.Replace(pattern, "\\?", ".", -1)
+		pattern = "^" + pattern + "$"
+
+		regex, err := regexp.Compile(pattern)
+
+		if err != nil {
+			panic(err)
+		}
+
+		result = append(result, *regex)
+	}
+
+	return result
+}
+
 func main() {
-	verbose = flag.Bool("v", false, "Verbose output")
-	apply = flag.Bool("f", false, "Apply changes")
+	flagVerbose = flag.Bool("v", false, "Verbose output")
+	flagApply = flag.Bool("f", false, "Apply changes")
+	flagExcludes = flag.String("e", ".bzr,CVS,.git,.hg,.svn", "Comma-separated list of excluded files and directories")
+	flagIncludes = flag.String("i", "", "Comma-separated list of included files, e.g \"*.js,*.html,*index.*\"")
 
 	flag.Usage = func() {
 		fmt.Println("Syntax: re [options] SEARCH REPLACEMENT [DIR ...]")
@@ -74,12 +130,15 @@ func main() {
 		dirs = append(dirs, ".")
 	}
 
-	if !*apply {
+	if !*flagApply {
 		fmt.Println("No changes will be applied unless -f is given.")
 	}
 
+	excludes = strings.Split(*flagExcludes, ",")
+	includes = parseRegexList(*flagIncludes)
+
 	for _, dir := range dirs {
-		if *verbose {
+		if *flagVerbose {
 			fmt.Println("Scanning", dir)
 		}
 
@@ -90,7 +149,7 @@ func main() {
 		}
 	}
 
-	if !*apply {
+	if !*flagApply {
 		fmt.Printf("%d file(s) WOULD have been updated.\n", replaced)
 	} else {
 		fmt.Printf("%d file(s) were updated.\n", replaced)
